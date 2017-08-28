fb5cea8c61b449349d89a32630aca2c27e2de63b
Geoff Golda <gcgolda@gmail.com>
2017-08-28 11:46:27 -0700

    added ScrollView to DetailActivity; moved API_KEY to gradle.properties

--- app/src/main/java/com/example/gcgol/popularmoviesv1/utilities/GlobalVariables.java
+++ app/src/main/java/com/example/gcgol/popularmoviesv1/utilities/GlobalVariables.java
@@ -1,13 +1,14 @@
 package com.example.gcgol.popularmoviesv1.utilities;
 
+import com.example.gcgol.popularmoviesv1.BuildConfig;
+
 /**
  * Created by gcgol on 25/08/2017.
  */
 
 public class GlobalVariables {
 
-    // TODO: place your MovieDB API key here
-    public static final String API_KEY = "";
+    public static final String API_KEY = BuildConfig.API_KEY;
 
     public static final String MOVIES_BASE_URL = "http://api.themoviedb.org/3/movie";
 
